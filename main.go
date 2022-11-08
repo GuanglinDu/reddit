0ff8a89fed05ad7e13bdcd79461262d6fde4a663
Guanglin Du <guanglin.du@gmail.com>
2022-11-08 08:02:49 +0800

    Create the code following the video tutorial

---
+++ main.go
@@ -0,0 +1,29 @@
+/**
+ * Created on Tue, 20221008 by Guanglin Du.
+ * Get Started with Go by Andrew Gerrand
+ * https://www.youtube.com/watch?v=2KmHtgtEZ1s&t=829s
+ */
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://reddit.com/r/golang.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(res.StatusOK)
+	}
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
